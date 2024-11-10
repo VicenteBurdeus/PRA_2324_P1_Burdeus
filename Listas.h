b7560a76f29b80104ba3d2ef75bd64630fe140d6
vicente burdeus sanchez <vbursan@upv.edu.es>
2024-11-10 19:07:46 +0100

    Añadida interfaz generica

---
+++ Listas.h
@@ -0,0 +1,16 @@
+#ifndef Listas_h
+#define Listas_h
+
+template <class T>
+class List{
+    public:
+        virtual void insert(int pos , T e) = 0;
+        virtual void append(T e) = 0;
+        virtual void prepend(T e) = 0;
+        virtual T remove(int pos) = 0;
+        virtual T get(int pos) = 0;
+        virtual int search(T e) = 0;
+        virtual bool empty() = 0;
+        virtual int size() = 0;     
+};
+#endif
