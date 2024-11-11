89b59f568794211cf93bba38ae69eddd6c01f0f7
vicente burdeus sanchez <vbursan@upv.edu.es>
2024-11-11 01:15:47 +0100

    Se a implementado las excepciones en caso de la el la clase shape el color no fuera ninguno de los que deveria

--- Shape.cpp
+++ Shape.cpp
@@ -5,7 +5,12 @@ Shape::Shape(){
 }
 
 Shape::Shape(std::string color){
+
+    if (color != "red" && color != "green" && color != "blue"){//aplicar Morgan ha servido de algo
+        throw std::invalid_argument("El color no es valido");
+    }else{
     this->color = color;
+    }
 }
 
 Shape::~Shape(){
@@ -16,6 +21,10 @@ std::string Shape::getColor() const {
 }
 
 void Shape::setColor(std::string c){
+    if (c != "red" && c != "green" && c != "blue"){//aplicar Morgan ha servido de algo
+        throw std::invalid_argument("El color no es valido");
+    }else{
     color = c;
+    }
 }
 
